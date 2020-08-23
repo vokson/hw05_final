28c3145e94cf0211dbb3b3d1a5a117dac28b1b00
Noskov Alexey <vokson@yandex.ru>
2020-08-23 05:50:44 +0300

    Ready for 2nd review

--- yatube/settings.py
+++ yatube/settings.py
@@ -107,29 +107,3 @@ EMAIL_BACKEND = 'django.core.mail.backends.filebased.EmailBackend'
 EMAIL_FILE_PATH = os.path.join(BASE_DIR, 'sent_emails')
 
 SITE_ID = 1
-
-# LOGGING = {
-#     'version': 1,
-#     'disable_existing_loggers': True,
-#     'formatters': {
-#         'simple': {
-#             'format': '[%(levelname)s:%(name)s:%(lineno)d] %(message)s'
-#         },
-#     },
-#     'handlers': {
-#         # ...
-#         'console': {
-#             'level': 'DEBUG',
-#             'class': 'logging.StreamHandler',
-#             'formatter': 'simple',
-#         },
-#     },
-#     'loggers': {
-#         # ...
-#         'django.db': {
-#             'handlers': ['console'],
-#             'level': 'DEBUG',  # <----<<<
-#             'propagate': False,
-#         }
-#     }
-# }
