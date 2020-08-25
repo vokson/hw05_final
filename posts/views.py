682d97a4d6a254cf0a415043b2660a6f8d479409
Noskov Alexey <vokson@yandex.ru>
2020-08-25 06:53:16 +0300

    Ready for 3rd review

--- posts/views.py
+++ posts/views.py
@@ -138,9 +138,8 @@ def follow_index(request):
 @login_required
 def profile_follow(request, username):
     author = get_object_or_404(User, username=username)
-    is_exist = Follow.objects.filter(user=request.user, author=author).exists()
-    if author != request.user and not is_exist:
-        Follow.objects.create(user=request.user, author=author)
+    if author != request.user:
+        Follow.objects.get_or_create(user=request.user, author=author)
 
     return redirect('profile', username=username)
 
